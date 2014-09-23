a3fad4ac235c85be50ec61b1e8ac36f03998105a
audi <audi.hsu@quantatw.com>
2014-09-23 02:35:37 -0700

    QuantaMesh 5000 Series T5032-LY6 - Form Factor: 1U height and 19" rack mountable - Port Configuration: 32 10/40GbE QSFP+ ports - Management ports: 1 RJ-45 console port and 1 out-of-band 10/100/1000-Mbps management port - Power Supply: Dual high efficiency redundant, hot swappable power supplies and front end access

--- components/all/platform-config/powerpc-quanta-ly6-p2020-r0/src/install/powerpc-quanta-ly6-p2020-r0.sh
+++ components/all/platform-config/powerpc-quanta-ly6-p2020-r0/src/install/powerpc-quanta-ly6-p2020-r0.sh
@@ -1,44 +1,33 @@
 ############################################################
 # <bsn.cl fy=2013 v=onl>
-#
-#        Copyright 2013, 2014 Big Switch Networks, Inc.
-#
+# 
+#        Copyright 2013, 2014 Big Switch Networks, Inc.       
+# 
 # Licensed under the Eclipse Public License, Version 1.0 (the
 # "License"); you may not use this file except in compliance
 # with the License. You may obtain a copy of the License at
-#
+# 
 #        http://www.eclipse.org/legal/epl-v10.html
-#
+# 
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific
 # language governing permissions and limitations under the
 # License.
-#
+# 
 # </bsn.cl>
 ############################################################
-#
-# These are the platforms to include in this installer build.
-#
 ############################################################
-INSTALLER_PLATFORMS :=  powerpc-as4600-54t \
-			powerpc-as5600-52x \
-			powerpc-as5610-52x \
-			powerpc-as5710-54x-r0a \
-			powerpc-as5710-54x-r0b \
-			powerpc-as6700-32x-r0 \
-			powerpc-as6700-32x-r0b \
-			powerpc-quanta-lb9a-r0 \
-			powerpc-quanta-lb9-r0 \
-			powerpc-quanta-ly2-r0 \
-			powerpc-quanta-ly6-p2020-r0 \
-			powerpc-quanta-ly6-p2041-r0 \
-			powerpc-dni-7448-r0
+# 
+# Installer scriptlet for the Quanta LY6 P2020. 
+#    
 
+# The boot command is to read the loader directly from the first partition and execute it. 
+platform_bootcmd='mmc part 0; fatload mmc 0:1 0x10000000 onl-loader; setenv bootargs console=$consoledev,$baudrate onl_platform=powerpc-quanta-ly6-p2020-r0; bootm 0x10000000'
 
-
-
-
-
+platform_installer() {
+    # Standard installation on the CF card.
+    installer_standard_blockdev_install mmcblk0 16M 64M ""
+}
 
